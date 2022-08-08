0c8139b8782670836e0c7a9fa675b65b80a49110
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-08-08 12:33:08 -0300

    Projeto criado

---
+++ hrwork/src/test/java/com/baratieri/hrwork/HrworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baratieri.hrwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
