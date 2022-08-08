3f33ec07080e62a013d3975a5e9fa31d95964980
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-08-08 14:11:40 -0300

    hr-worker implementado

---
+++ hrwork/src/main/java/com/baratieri/hrwork/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.baratieri.hrwork.repositories;
+
+import com.baratieri.hrwork.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
