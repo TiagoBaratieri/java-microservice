7a71425052241db764e586017cfd8e8b017b2fb7
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-08-08 16:08:53 -0300

    Fein client

--- hrpayroll/src/main/java/com/baratieri/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/com/baratieri/hrpayroll/services/PaymentService.java
@@ -2,30 +2,19 @@ package com.baratieri.hrpayroll.services;
 
 import com.baratieri.hrpayroll.entities.Payment;
 import com.baratieri.hrpayroll.entities.Worker;
+import com.baratieri.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
-    public Payment getPayment(Long WorkerId, Integer days) {
+    public Payment getPayment(Long workerId, Integer days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-
-        uriVariables.put("id", "" + WorkerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
