910e49a8696d4d6697f98c20a04cacf0c64df83c
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-08-09 11:25:45 -0300

    hr-eureka-server criado

---
+++ demohr-eureka-server/src/main/java/com/baratieri/demohreurekaserver/DemohrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.baratieri.demohreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohrEurekaServerApplication.class, args);
+	}
+
+}
---
+++ demohr-eureka-server/src/test/java/com/baratieri/demohreurekaserver/DemohrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baratieri.demohreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
